5923eddc959975df8399b361f5b6d245941d63bc
seongjin_song <seongjin_song@NB>
2020-04-24 22:48:47 +0900

    [add]test
    - chapter2

---
+++ src/test/java/com/aws/websvc/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.aws.websvc.dto;
+import com.aws.websvc.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombokTest(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
